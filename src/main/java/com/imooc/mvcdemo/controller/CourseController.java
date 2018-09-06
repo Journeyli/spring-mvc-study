70dd54e7decabe7c7ec2f34c3991b1b0b9d51033
Journeyli <243124014@qq.com>
2018-09-06 12:28:03 +0900

    version 1.0

---
+++ src/main/java/com/imooc/mvcdemo/controller/CourseController.java
@@ -0,0 +1,148 @@
+package com.imooc.mvcdemo.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.imooc.mvcdemo.model.Course;
+import com.imooc.mvcdemo.service.CourseService;
+
+@Controller
+@RequestMapping("/courses")
+// /courses/**
+public class CourseController {
+
+	private static Logger log = LoggerFactory.getLogger(CourseController.class);
+	private CourseService courseService;
+
+	@Autowired
+	public void setCourseService(CourseService courseService) {
+		this.courseService = courseService;
+	}
+
+	// 本方法将处理 /courses/view?courseId=123
+	@RequestMapping(value = "/view", method = RequestMethod.GET)
+	public String viewCourses(@RequestParam("courseId") Integer courseId, Model model) {
+		log.debug("In viewCourse, courseId = {}", courseId);
+		Course course = courseService.getCoursebyId(courseId);
+		model.addAttribute(course);
+		return "course_overview";
+	}
+
+	// 本方法将处理 /courses/view2/{courseId}
+	@RequestMapping(value = "/view2/{courseId}", method = RequestMethod.GET)
+	public String viewCourses2(@PathVariable("courseId") Integer courseId, Map<String, Object> model) {
+		log.debug("In viewCourse2, courseId = {}", courseId);
+		Course course = courseService.getCoursebyId(courseId);
+		model.put("course", course);
+		return "course_overview";
+	}
+
+	// /courses/view3?courseId=456
+	@RequestMapping("view3")
+	public String viewCourses3(HttpServletRequest request) {
+		Integer courseId = Integer.valueOf(request.getParameter("courseId"));
+		log.debug("In viewCourse3, courseId = {}", courseId);
+		Course course = courseService.getCoursebyId(courseId);
+		request.setAttribute("course", course);
+		return "course_overview";
+	}
+
+	@RequestMapping(value = "/admin", method = RequestMethod.GET, params = "add")
+	public String createCourse() {
+		return "course_admin/edit";
+
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String doSave(@ModelAttribute Course course) {
+
+		log.debug("Info of Course");
+		log.debug(ReflectionToStringBuilder.toString(course));
+		// 在此进行业务操作，如数据库持久化操作
+		course.setCourseId(123);
+		return "redirect:view2/" + course.getCourseId();
+
+	}
+
+	@RequestMapping(value = "/upload", method = RequestMethod.GET)
+	public String showUploadPage() {
+		return "course_admin/file";
+	}
+
+	@RequestMapping(value = "/doUpload", method = RequestMethod.POST)
+	public String doUploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+		if (!file.isEmpty()) {
+			log.debug("Process file:{}", file.getOriginalFilename());
+			FileUtils.copyInputStreamToFile(file.getInputStream(),
+					new File("c:\\temp\\imooc\\", System.currentTimeMillis() + file.getOriginalFilename()));
+		}
+		return "success";
+	}
+
+	@RequestMapping(value = "/{courseId}", method = RequestMethod.GET)
+	public @ResponseBody Course getCourseInJson(@PathVariable Integer courseId) {
+		return courseService.getCoursebyId(courseId);
+	}
+
+	@RequestMapping(value = "/jsontype/{courseId}", method = RequestMethod.GET)
+	public ResponseEntity<Course> getCourseInJson2(@PathVariable Integer courseId) {
+		Course course = courseService.getCoursebyId(courseId);
+		return new ResponseEntity<Course>(course, HttpStatus.OK);
+	}
+	// http://localhost:8080/courses/baseType.do?age=10
+	@RequestMapping(value="baseType.do")
+	@ResponseBody
+	public String baseType(int age) {
+		return "age = "+age;
+	}
+	// http://localhost:8080/courses/baseType2.do?xage=10
+	@RequestMapping(value="baseType2.do")
+	@ResponseBody
+	public String baseType2(@RequestParam("xage") int age) {
+		return "age = "+age;
+	}
+	// http://localhost:8080/courses/baseType3.do
+	// http://localhost:8080/courses/baseType3.do?age=10
+	@RequestMapping(value="baseType3.do")
+	@ResponseBody
+	public String baseType3(Integer age) {
+		return "age = "+age;
+	}
+	
+	// http://localhost:8080/courses/array.do?name=Ann&name=Tom&name=Jhon
+	@RequestMapping(value="array.do")
+	@ResponseBody
+	public String array(String[] name) {
+		StringBuilder sbf =new StringBuilder();
+		for(String item : name) {
+			sbf.append(item).append(" ");
+		}
+		return sbf.toString();
+	}
+	// http://localhost:8080/courses/object.do?courseId=1000
+	@RequestMapping(value="object.do")
+	@ResponseBody
+	public String object(Course course) {
+		return "CourseID = "+ course.getCourseId();
+	}
+}
